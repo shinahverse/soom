bba8846bfd1f43b804b3d8c275e9063794450092
시나버스 <131946130+shinahverse@users.noreply.github.com>
2023-11-29 09:47:13 +0900

    socket.io 3

--- src/server.js
+++ src/server.js
@@ -13,5 +13,9 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+wsServer.on("connection", (socket)=>{
+    console.log(socket);
+});
+
 const handleListen = ()=> console.log("listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
