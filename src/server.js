0cd69b1e676ea91d02e0d5d3170503007a4510c4
시나버스 <131946130+shinahverse@users.noreply.github.com>
2023-11-29 09:32:48 +0900

    soket.io 1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import http from "http";
-import WebSocket from "ws";
 import express from 'express';
 
 const app = express();
@@ -10,34 +9,7 @@ app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("연결중 http://localhost:3000");
-//app.listen(3000, handleListen);
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
-const sockets = [];
 
-wss.on("connection", (socket)=>{
-    sockets.push(socket);
-    socket["nickname"] = "익명";
-    console.log("브라우저와 연결되었습니다.");
-    socket.on("close", ()=> console.log("서버>서버와 연결이 끊겼습니다."));
-    // socket.on("message", (message)=>{
-    socket.on("message", (msg)=>{
-        const message = JSON.parse(msg);
-        // console.log(message.type, message.payload);
-        // console.log(`[서버] ${message}`);
-        // socket.send(`[서버] ${message}`);
-        //sockets.forEach(aSocket => aSocket.send(`${message}`));
-        switch(message.type){
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${message.payload}`));
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-        }
-    });
-    // socket.send("hello!");
-});
+const handleListen = ()=> console.log("listening on http://localhost:3000");
 server.listen(3000, handleListen);
