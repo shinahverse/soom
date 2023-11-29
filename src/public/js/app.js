becff54b21f6ff6e847b43e8f1f2276578ad0420
시나버스 <131946130+shinahverse@users.noreply.github.com>
2023-11-29 10:44:39 +0900

    채팅룸 만들기 2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -6,15 +6,20 @@ const room = document.getElementById("room");
 
 room.hidden = true;
 
+let roomName;
+
 function showRoom(){
     welcome.hidden = true;
     room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `이 방의 이름은 ${roomName}`;
 }
 
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = form.querySelector("input");
     socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 }
 
