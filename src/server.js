ec6fadd9bcee76d988a54360d9a206ce1f42a66d
시나버스 <131946130+shinahverse@users.noreply.github.com>
2023-11-29 09:42:36 +0900

    socket.io 2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from "http";
+import SocketIO from "socket.io";
 import express from 'express';
 
 const app = express();
@@ -9,7 +10,8 @@ app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
 const handleListen = ()=> console.log("listening on http://localhost:3000");
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
